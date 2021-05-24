2547e7aae93608588dc2b40b76869d608c277809
I-AM-INSANE <vasiliyqqerty@gmail.com>
2021-05-24 20:58:40 +0300

    Colliders update

---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    #region Fields
+
+    [SerializeField] private float mouseSensitivity = 100f;
+
+    private Transform player;
+    private float xRotation = 0;
+
+    #endregion
+
+    #region Methods
+
+    private void Awake()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        player = transform.parent;
+    }
+    private void FixedUpdate()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.fixedDeltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.fixedDeltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        player.Rotate(Vector3.up * mouseX);
+    }
+
+    #endregion
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    #region Fields
+
+    [SerializeField] private float moveSpeed = 5f;
+
+    private CharacterController controller;
+
+    #endregion
+
+    #region Methods
+
+    private void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 moveDirection = transform.right * x + transform.forward * z;
+        controller.Move(moveDirection * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    #endregion
+}
